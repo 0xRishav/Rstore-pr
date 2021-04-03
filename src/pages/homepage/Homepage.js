e4a4df315a4421adc64a170fcf6bebf8f3dc8035
Rishav Bharti <rshvbharti@gmail.com>
2021-04-03 10:39:18 +0530

    added and completed homepage UI

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,142 @@
+import React from "react";
+import { BuyNowTextLinks, ImageSlider, Navbar } from "../../components";
+import { SiOneplus, SiHp } from "react-icons/si";
+import { AiFillApple } from "react-icons/ai";
+import "./Homepage.css";
+import { Link } from "react-router-dom";
+
+function Homepage() {
+  const bannerImages = [
+    {
+      imageUrl:
+        "https://www.spark.co.nz/content/dam/telecomcms/responsive/images/shop/apple/iphone-12-pro/buy-now/buy-now-consumer/iphone12-buy-now-banner-mobile.jpg",
+      productUrl: "/product/218b86a4-d7bb-495b-a5a5-5334fac1c606",
+    },
+    {
+      imageUrl:
+        "https://theaxo.com/wp-content/uploads/2020/10/OnePlus-8T-1024x576.jpg",
+      productUrl: "/product/614e6ceb-f887-4991-8576-ade4177f3869",
+    },
+    {
+      imageUrl:
+        "https://about.att.com/ecms/dam/snr/2020/February2020/StoryLevelBanner/01.24.2020_Samsung%20Device-combo_STORY_LEVEL_BANNER_1600x483.jpg",
+      productUrl: "/product/379318e2-aebf-4079-8882-42cb158be836",
+    },
+  ];
+  return (
+    <div className="Homepage">
+      <Navbar />
+      <ImageSlider images={bannerImages} parent="homepage" />
+      <h2 className="Homepage__subHeading">Featured Products</h2>
+      <div className="Homepage__featuredProductsWrapper">
+        <div className="Homepage__featuredProductContainer">
+          <img
+            className="Homepage__featuredProdImage"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/a71331107041495.5fac2c616f3ae.jpg"
+            alt="oneplus-phone"
+          />
+          <div className="Homepage__infoContainer">
+            <div className="Homepage__featuredProdName">
+              <SiOneplus color="red" className="Homepage__featuredProdLogo" />
+              OnePlus 8T
+            </div>
+            <BuyNowTextLinks link="/product/614e6ceb-f887-4991-8576-ade4177f3869" />
+          </div>
+        </div>
+
+        <div className="Homepage__featuredProductContainer">
+          <div className="Homepage__infoContainer">
+            <div className="Homepage__featuredProdName">
+              <AiFillApple
+                color="#CECECE"
+                className="Homepage__featuredProdLogo"
+              />
+              iPhone 11
+            </div>
+            <BuyNowTextLinks link="/product/bb463b8b-b76c-4f6a-9726-65ab5730b69b" />
+          </div>
+          <img
+            className="Homepage__featuredProdImage"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/230a8486312713.5d9580970f876.jpg"
+            alt="iphone11"
+          />
+        </div>
+
+        <div className="Homepage__featuredProductContainer">
+          <img
+            className="Homepage__featuredProdImage"
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/131e2099947973.5efde4c45921a.jpg"
+            alt="oneplus-phone"
+          />
+          <div className="Homepage__infoContainer">
+            <div className="Homepage__featuredProdName">
+              <SiHp color="#0091CF" className="Homepage__featuredProdLogo" />
+              HP Spectre x360
+            </div>
+            <BuyNowTextLinks link="/product/c89e14b7-af05-4c38-865c-a607c295adb2" />
+          </div>
+        </div>
+      </div>
+
+      <div className="Homepage__featuredProductContainer">
+        <div className="Homepage__infoContainer">
+          <div className="Homepage__featuredProdName">
+            <AiFillApple
+              color="#CECECE"
+              className="Homepage__featuredProdLogo"
+            />
+            Apple MacBook Pro
+          </div>
+          <BuyNowTextLinks link="/product/f732eae8-1a75-4875-9023-11730c4e6054" />
+        </div>
+        <img
+          className="Homepage__featuredProdImage"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0a175488400949.5dd52df927373.jpg"
+          alt="macbookpro"
+        />
+      </div>
+
+      <h2 className="Homepage__subHeading">Categories</h2>
+      <div className="Homepage__categoryWrapper">
+        <div className="Homepage__categoryContainer">
+          <img
+            src="https://fdn.gsmarena.com/imgroot/news/20/08/huawei-y9a-specs-design-ad-banner/-1220x526/gsmarena_004.jpg"
+            alt="mobiles-banner"
+            className="Homepage__categoryImage"
+          />
+          <h3>Mobile</h3>
+          <BuyNowTextLinks link="" />
+        </div>
+        <div className="Homepage__categoryContainer">
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3c018587711043.5dc0b3f75a8ca.jpg"
+            alt="laptop-banner"
+            className="Homepage__categoryImage"
+          />
+          <h3>Laptop</h3>
+          <BuyNowTextLinks link="" />
+        </div>
+        <div className="Homepage__categoryContainer">
+          <img
+            src="https://oasis.opstatics.com/content/dam/oasis/page/store/tv/new/New-TV_Banner_1600x1200.jpg"
+            alt="TV-banner"
+            className="Homepage__categoryImage"
+          />
+          <h3>TV</h3>
+          <BuyNowTextLinks link="" />
+        </div>
+        <div className="Homepage__categoryContainer">
+          <img
+            src="https://cdn.dribbble.com/users/1146422/screenshots/6781898/main.png?compress=1&resize=800x600"
+            alt=""
+            className="Homepage__categoryImage"
+          />
+          <h3>Smart Watch</h3>
+          <BuyNowTextLinks link="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
