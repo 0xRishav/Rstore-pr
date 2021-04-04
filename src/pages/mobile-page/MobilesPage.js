81b03ee8c172b69c6c00ba22afbc16cf04e41227
Rishav Bharti <rshvbharti@gmail.com>
2021-04-04 23:40:56 +0530

    Added mobiles page

---
+++ src/pages/mobile-page/MobilesPage.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { Loader, Product } from "../../components";
+import { ProductsContext } from "../../contexts/productsContext";
+import "./MobilesPage.css";
+
+function MobilesPage() {
+  const { isLoading, filteredData, dispatch } = useContext(
+    ProductsContext
+  ).products;
+  const Mobiles = filteredData.filter(
+    (product) => product.category === "Mobiles"
+  );
+  return (
+    <div className="MobilesPage">
+      {isLoading && <Loader />}
+      <div className="products-wrapper">
+        {Mobiles.map((product) => (
+          <Product {...product} dispatch={dispatch} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MobilesPage;
