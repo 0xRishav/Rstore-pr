41dbdcb3d4c5360757eb61e6af0ab7b2b492ef02
Rishav Bharti <rshvbharti@gmail.com>
2021-04-04 23:41:21 +0530

    Added TV page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export { default as WishlistPage } from "./wishlist-page/WishlistPage";
 export { default as Homepage } from "./homepage/Homepage";
 export { default as CartPage } from "./cartpage/CartPage";
 export { default as AllProductsPage } from "./all-products-page/AllProductsPage";
+export { default as TVPage } from "./tv-page/TVPage";
+export { default as MobilesPage } from "./mobile-page/MobilesPage";
