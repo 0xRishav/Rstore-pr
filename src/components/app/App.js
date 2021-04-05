21feb1730b3450723e90a0563f2dbd5a6cc7b0c0
Rishav Bharti <rshvbharti@gmail.com>
2021-04-05 11:10:25 +0530

    Added scroll to the top feature after routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 // import { Switch, Route } from "react-router-dom";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
 import { Navbar } from "..";
+import { _ScrollToTop } from "../../helpers";
 import {
   AllProductsPage,
   CartPage,
@@ -17,39 +18,43 @@ import ProductPage from "../product-page/ProductPage";
 import "./App.css";
 
 function App() {
+  const ScrollToTop = withRouter(_ScrollToTop);
+
   return (
     <div className="App__wrapper">
       <Navbar />
       <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <div className="App">
-          <Route path="/products">
-            <AllProductsPage />
+        <ScrollToTop>
+          <Route exact path="/">
+            <Homepage />
           </Route>
-          <Route path="/wishlist">
-            <WishlistPage />
-          </Route>
-          <Route path="/cart">
-            <CartPage />
-          </Route>
-          <Route path="/tv">
-            <TVPage />
-          </Route>
-          <Route path="/mobile">
-            <MobilesPage />
-          </Route>
-          <Route path="/laptop">
-            <LaptopsPage />
-          </Route>
-          <Route path="/watch">
-            <WatchPage />
-          </Route>
-          <Route path="/product/:id">
-            <ProductPage />
-          </Route>
-        </div>
+          <div className="App">
+            <Route path="/products">
+              <AllProductsPage />
+            </Route>
+            <Route path="/wishlist">
+              <WishlistPage />
+            </Route>
+            <Route path="/cart">
+              <CartPage />
+            </Route>
+            <Route path="/tv">
+              <TVPage />
+            </Route>
+            <Route path="/mobile">
+              <MobilesPage />
+            </Route>
+            <Route path="/laptop">
+              <LaptopsPage />
+            </Route>
+            <Route path="/watch">
+              <WatchPage />
+            </Route>
+            <Route path="/product/:id">
+              <ProductPage />
+            </Route>
+          </div>
+        </ScrollToTop>
       </Switch>
     </div>
   );
---
+++ src/helpers/index.js
@@ -0,0 +1,1 @@
+export { default as _ScrollToTop } from "./_ScrollToTop";
