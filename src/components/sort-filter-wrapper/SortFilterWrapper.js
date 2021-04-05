748d17b682fb6ba657e0f0889e84ce89d8670e84
Rishav Bharti <rshvbharti@gmail.com>
2021-04-05 09:03:24 +0530

    Refactor: added name to sort and filter button

--- src/components/sort-filter-wrapper/SortFilterWrapper.js
+++ src/components/sort-filter-wrapper/SortFilterWrapper.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { AiFillCaretDown } from "react-icons/ai";
 import { FilterDropDown, SortDropdown } from "..";
+import { ProductsContext } from "../../contexts/productsContext";
 import "./SortFilterWrapper.css";
 
 function SortFilterWrapper() {
+  const {
+    showFastDeliveryOnly,
+    showFreeShippingOnly,
+    filterPrice,
+    sortBy,
+  } = useContext(ProductsContext).products;
   const [showFilterDropDown, setShowFilterDropDown] = useState(false);
   const [showSortDropDown, setShowSortDropDown] = useState(false);
 
   const filterDropDownClickHandler = () => {
     if (showSortDropDown === true) {
       setShowSortDropDown(false);
-    }
+  }
     setShowFilterDropDown(!showFilterDropDown);
   };
   const sortDropDownClickHandler = () => {
@@ -28,19 +35,24 @@ function SortFilterWrapper() {
           className="SortFilterWrapper__filterDropDownBtn"
           onClick={filterDropDownClickHandler}
         >
-          None
+          {showFastDeliveryOnly || showFreeShippingOnly ? "Tags" : "None"}
+          {filterPrice ? " & Price" : ""}
           <AiFillCaretDown />
         </button>
         {showFilterDropDown && <FilterDropDown />}
       </div>
 
       <div className="SortFilterWrapper__sortDropDownContainer">
-        <h4 className="SortFilterWrapper__sortHeading">Sort:</h4>
+        <h4 className="SortFilterWrapper__sortHeading">Sort by Price:</h4>
         <button
           className="SortFilterWrapper__sortDropDownBtn"
           onClick={sortDropDownClickHandler}
         >
-          None
+          {sortBy
+            ? sortBy === "LOW_TO_HIGH"
+              ? "Low To High"
+              : "High To Low"
+            : "None"}
           <AiFillCaretDown />
         </button>
         {showSortDropDown && <SortDropdown />}
