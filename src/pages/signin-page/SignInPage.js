9657030c08df9823d39699b28dfe8a22dc6857dc
Rishav Bharti <rshvbharti@gmail.com>
2021-04-17 09:32:58 +0530

    Created signin page

--- src/pages/signin-page/SignInPage.js
+++ src/pages/signin-page/SignInPage.js
@@ -1,20 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import "./SignInPage.css";
 import { BsArrowUpRight } from "react-icons/bs";
+import { authContext } from "../../contexts/authContext";
 
 function SignInPage() {
+  const { loginWithCredentials } = useContext(authContext);
+  const { state } = useLocation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+
+  async function signinHandler() {
+    const response = await loginWithCredentials(email, password);
+    if (response.success) {
+      history.push(state?.from ? state.from : "/");
+    }
+  }
+
+  console.log({ state });
+
   return (
     <div className="signin">
       <h2>Sign in to RStore</h2>
       <div className="signin__inputContainer">
-        <input type="username" placeholder="Username" />
-        <input type="password" placeholder="Password" />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      <button className="blue-btn--primary">Sign in</button>
+      <button className="blue-btn--primary" onClick={signinHandler}>
+        Sign in
+      </button>
       <div>
         Do not have an RStore account?{" "}
-        <Link>
+        <Link to="/signup">
           Create yours now <BsArrowUpRight />{" "}
         </Link>
       </div>
