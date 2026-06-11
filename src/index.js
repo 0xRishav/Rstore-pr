08ab7fa8bd3755be34a24288a81d7ebe384eb122
0xRishav <rshvbharti@gmail.com>
2026-06-11 19:16:57 +0530

    feat: upgrade React 17 to 19 with createRoot entry point
    - Bump react and react-dom to ^19.2.7
    - Replace ReactDOM.render with createRoot API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./components";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +7,7 @@ import { ProductsContextProvider } from "./contexts/productsContext";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/authContext";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <ProductsContextProvider>
@@ -16,11 +16,7 @@ ReactDOM.render(
         </BrowserRouter>
       </ProductsContextProvider>
     </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
