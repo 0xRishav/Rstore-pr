8acddb3a726107fcec6c93df15ec117978339a6a
0xRishav <rshvbharti@gmail.com>
2026-06-12 17:13:18 +0530

    refactor: extract homepage data to separate file

---
+++ src/data/homepage.js
@@ -0,0 +1,78 @@
+export const featuredProducts = [
+  {
+    name: "OnePlus 8T",
+    imgSrc:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/a71331107041495.5fac2c616f3ae.jpg",
+    alt: "one-plus-phone",
+    logo: "oneplus",
+    link: "/product/6091ff73fd79302e84dfe81b",
+  },
+  {
+    name: "iPhone 11",
+    imgSrc:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/disp/230a8486312713.5d9580970f876.jpg",
+    alt: "iphone-11",
+    logo: "apple",
+    link: "/product/6091ff73fd79302e84dfe816",
+  },
+  {
+    name: "HP Spectre x360",
+    imgSrc:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400/131e2099947973.5efde4c45921a.jpg",
+    alt: "HP-Spectre-x360",
+    logo: "hp",
+    link: "/product/6091ff73fd79302e84dfe839",
+  },
+  {
+    name: "Apple Macbook pro",
+    imgSrc:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0a175488400949.5dd52df927373.jpg",
+    alt: "Apple-Macbook-pro",
+    logo: "apple",
+    link: "/product/6091ff73fd79302e84dfe83a",
+  },
+];
+
+export const bannerImages = [
+  {
+    imageUrl:
+      "https://theaxo.com/wp-content/uploads/2020/10/OnePlus-8T-1024x576.jpg",
+    productUrl: "/product/6091ff73fd79302e84dfe81b",
+  },
+  {
+    imageUrl:
+      "https://images.samsung.com/is/image/samsung/assets/in/smartphones/galaxy-s20/galaxy-s20-fe/Homepage_banner1440.jpg",
+    productUrl: "/product/6091ff73fd79302e84dfe820",
+  },
+];
+
+export const categories = [
+  {
+    name: "Mobile",
+    imgSrc:
+      "https://fdn.gsmarena.com/imgroot/news/20/08/huawei-y9a-specs-design-ad-banner/-1220x526/gsmarena_004.jpg",
+    alt: "mobiles-banner",
+    link: "/category/Mobiles",
+  },
+  {
+    name: "Laptop",
+    imgSrc:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3c018587711043.5dc0b3f75a8ca.jpg",
+    alt: "laptops-banner",
+    link: "/category/Laptop",
+  },
+  {
+    name: "TV",
+    imgSrc:
+      "https://oasis.opstatics.com/content/dam/oasis/page/store/tv/new/New-TV_Banner_1600x1200.jpg",
+    alt: "TV-banner",
+    link: "/category/TV",
+  },
+  {
+    name: "Smart Watch",
+    imgSrc:
+      "https://cdn.dribbble.com/users/1146422/screenshots/6781898/main.png?compress=1&resize=800x600",
+    alt: "Watch-banner",
+    link: "/category/Watch",
+  },
+];
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,104 +1,17 @@
 import React from "react";
-import { BuyNowTextLinks, ImageSlider } from "../../components";
 import { SiOneplus, SiHp } from "react-icons/si";
 import { AiFillApple } from "react-icons/ai";
+import { BuyNowTextLinks, ImageSlider } from "../../components";
 import "./Homepage.css";
-import { useWindowDimensions } from "../../custom-hooks";
-import { useProduct } from "../../helpers";
+import { featuredProducts, bannerImages, categories } from "../../data/homepage";
+
+const logoMap = {
+  oneplus: <SiOneplus color="red" className="Homepage__featuredProdLogo" />,
+  hp: <SiHp color="#0091CF" className="Homepage__featuredProdLogo" />,
+  apple: <AiFillApple color="black" className="Homepage__featuredProdLogo" />,
+};
 
 function Homepage() {
-  const { products } = useProduct();
-  const { width } = useWindowDimensions();
-
-  const featuredProducts = [
-    {
-      name: "OnePlus 8T",
-      imgSrc:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/a71331107041495.5fac2c616f3ae.jpg",
-      alt: "one-plus-phone",
-      logo: <SiOneplus color="red" className="Homepage__featuredProdLogo" />,
-      link: "/product/6091ff73fd79302e84dfe81b",
-    },
-    {
-      name: "iPhone 11",
-      imgSrc:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/230a8486312713.5d9580970f876.jpg",
-      alt: "iphone-11",
-      logo: (
-        <AiFillApple color="black" className="Homepage__featuredProdLogo" />
-      ),
-      link: "/product/6091ff73fd79302e84dfe816",
-    },
-    {
-      name: "HP Spectre x360",
-      imgSrc:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/1400/131e2099947973.5efde4c45921a.jpg",
-      alt: "HP-Spectre-x360",
-      logo: <SiHp color="#0091CF" className="Homepage__featuredProdLogo" />,
-      link: "/product/6091ff73fd79302e84dfe839",
-    },
-    {
-      name: "Apple Macbook pro",
-      imgSrc:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/0a175488400949.5dd52df927373.jpg",
-      alt: "Apple-Macbook-pro",
-      logo: (
-        <AiFillApple color="black" className="Homepage__featuredProdLogo" />
-      ),
-      link: "/product/6091ff73fd79302e84dfe83a",
-    },
-  ];
-
-  const bannerImages = [
-    // {
-    //   imageUrl:
-    //     width > 754
-    //       ? "https://www.spark.co.nz/content/dam/telecomcms/responsive/images/shop/apple/iphone-12-pro/buy-now/buy-now-consumer/iphone12-buy-now-banner-mobile.jpg"
-    //       : "https://www.apple.com/v/home/x/images/heroes/iphone-12-pro/iphone_12_pro_us__e5oyysg4k0ya_small_2x.jpg",
-    //   productUrl: "/product/2",
-    // },
-    {
-      imageUrl:
-        "https://theaxo.com/wp-content/uploads/2020/10/OnePlus-8T-1024x576.jpg",
-      productUrl: "/product/6091ff73fd79302e84dfe81b",
-    },
-    {
-      imageUrl:
-        "https://images.samsung.com/is/image/samsung/assets/in/smartphones/galaxy-s20/galaxy-s20-fe/Homepage_banner1440.jpg",
-      productUrl: "/product/6091ff73fd79302e84dfe820",
-    },
-  ];
-
-  const categories = [
-    {
-      name: "Mobile",
-      imgSrc:
-        "https://fdn.gsmarena.com/imgroot/news/20/08/huawei-y9a-specs-design-ad-banner/-1220x526/gsmarena_004.jpg",
-      alt: "mobiles-banner",
-      link: "/category/Mobiles",
-    },
-    {
-      name: "Laptop",
-      imgSrc:
-        "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3c018587711043.5dc0b3f75a8ca.jpg",
-      alt: "laptops-banner",
-      link: "/category/Laptop",
-    },
-    {
-      name: "TV",
-      imgSrc:
-        "https://oasis.opstatics.com/content/dam/oasis/page/store/tv/new/New-TV_Banner_1600x1200.jpg",
-      alt: "TV-banner",
-      link: "/category/TV",
-    },
-    {
-      name: "Smart Watch",
-      imgSrc:
-        "https://cdn.dribbble.com/users/1146422/screenshots/6781898/main.png?compress=1&resize=800x600",
-      alt: "Watch-banner",
-      link: "/category/Watch",
-    },
-  ];
 
   return (
     <div className="Homepage">
@@ -116,7 +29,7 @@ function Homepage() {
             />
             <div className="Homepage__infoContainer">
               <div className="Homepage__featuredProdName">
-                {product.logo}
+                {logoMap[product.logo]}
                 {product.name}
               </div>
               <BuyNowTextLinks link={product.link} />
