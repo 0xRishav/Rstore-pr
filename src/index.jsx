5877e8c2fac2f09aba60880ab82b9311f91d9bcf
0xRishav <rshvbharti@gmail.com>
2026-06-12 17:45:39 +0530

    feat: add button system, form primitives, and animation utilities

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/tokens.css";
 import "./styles/base.css";
+import "./styles/animations.css";
+import "./styles/components.css";
 import { App, ErrorBoundary } from "./components";
 import { ProductsContextProvider } from "./contexts/productsContext";
 import { CartContextProvider } from "./contexts/CartContext";
