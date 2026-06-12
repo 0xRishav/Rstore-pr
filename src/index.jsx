e06234a6296e2e89f5d9a233fe9ab7ea6ba499ad
0xRishav <rshvbharti@gmail.com>
2026-06-12 17:45:10 +0530

    feat: add design token system with custom properties and Inter font

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/tokens.css";
+import "./styles/base.css";
 import { App, ErrorBoundary } from "./components";
 import { ProductsContextProvider } from "./contexts/productsContext";
 import { CartContextProvider } from "./contexts/CartContext";
